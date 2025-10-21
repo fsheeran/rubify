0c75580cd6503341a5e81c3a350875072de44d3c
fsheeran <fiadhsheeran@gmail.com>
2025-10-20 21:40:57 -0700

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+	plugins: [sveltekit()],
 	resolve: process.env.VITEST
 		? {
 				conditions: ['browser']
